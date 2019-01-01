971426228bb746167a43c38ab88daa298a24fde3
Hassan Khalaila <hkh148@gmail.com>
2019-01-01 17:29:09 +0200

    updated spotter

--- Spotter.py
+++ Spotter.py
@@ -8,16 +8,28 @@ from macros import *
 mydb = mysql.connector.connect(host='localhost', user=USER_NAME, passwd=PASSWORD,database='projectdb')
 mycursor = mydb.cursor()
 exclude = set(string.punctuation)
+exclude.remove("'")
+
+abbrev_dict = {"ר'":"רבי","דר'":"דרבי","ור'":"ורבי"}
+def open_abbreviation(word):
+	if word in abbrev_dict.keys():
+		return abbrev_dict[word]
+	return word
 
 input_file = open(sys.argv[1],'r',encoding='utf8')
 result = open(sys.argv[2],'w',encoding='utf8')
 csv_writer = csv.writer(result,dialect='excel',lineterminator='\n')
 csv_writer.writerow(['Mention','Start index','End index','Title','Wikipedia link'])
 data = input_file.read()
-for i in range(1,5):
+for i in range(1,4):
 	grams = ngrams(data.split(), i)
 	start_index = 0
 	for gram in grams:
+		if i > 1:
+			for j in range(2,i+1):
+				if gram[j-1] in abbrev_dict.keys() or gram[j-1] in abbrev_dict.items():
+					continue;
+		gram[0] = open_abbreviation(gram[0])
 		word = ''.join([w + ' ' for w in gram]).strip()
 		word = ''.join(ch for ch in word if ch not in exclude)
 		if word.find('–') != -1:
